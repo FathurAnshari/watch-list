b8e41e9415b76fc29027f22349dc10099e9080cb
FathurAnshari <anshariF17@gmail.com>
2022-05-30 22:47:51 +0700

    innit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import { Watched } from "./pages/Watched";
+import { WatchList } from "./pages/WatchList";
+import { Add } from "./pages/Add";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<WatchList />} />
+      <Route path="watched" element={<Watched />} />
+      <Route path="add" element={<Add />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/WatchList.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const WatchList = () => {
+  return <div>WatchList</div>;
+};
